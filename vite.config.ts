54514304660439644c3fd66b8167e9074c995116
idealase <brodie.sandford@gmail.com>
2025-12-23 16:09:20 +0800

    base me-net for ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/me-net/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
